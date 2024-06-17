3c54224b1008eb5db9fff39d3a66ab3c86f85934
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-17 13:14:42 +0200

    add: casual number in array

---
+++ js/myjs.js
@@ -0,0 +1,10 @@
+let container = document.querySelector('.container');
+
+// creo i 5 numeri casuali
+const arrNumeri = [];
+while(arrNumeri.length < 5){
+    let num = Math.floor(Math.random() * 101)
+    if(!arrNumeri.includes(num)){
+        arrNumeri.push(num)
+    }
+}
