dde52f354b5ed120057e1c3db88c60cabe656677
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-17 13:26:05 +0200

    add: number create dynamically

--- js/myjs.js
+++ js/myjs.js
@@ -6,5 +6,9 @@ while(arrNumeri.length < 5){
     let num = Math.floor(Math.random() * 101)
     if(!arrNumeri.includes(num)){
         arrNumeri.push(num)
+        container.innerHTML += `<span class="number">${num}</span>`
+        
     }
 }
+
+
